770e6f450ec91fd4d86b531a83d34c9eb6781f52
Justina V <justina.varoneckaite@teltonika.lt>
2024-11-20 15:54:23 +0200

    Update 16 files
    - /tuya/files/tuyadevicemonitor.config
    - /tuya/files/tuyadevicemonitor.init
    - /tuya/src/.vscode/settings.json
    - /tuya/src/include/argp_parse.h
    - /tuya/src/include/daemon.h
    - /tuya/src/include/params_json.h
    - /tuya/src/include/sys_param.h
    - /tuya/src/include/tuya_functions.h
    - /tuya/src/src/argp_parse.c
    - /tuya/src/src/daemon.c
    - /tuya/src/src/main.c
    - /tuya/src/src/params_json.c
    - /tuya/src/src/sys_param.c
    - /tuya/src/src/tuya_functions.c
    - /tuya/src/Makefile
    - /tuya/Makefile

--- tuya/src/src/params_json.c
+++
@@ -1,8 +0,0 @@
-#include "params_json.h"
-
-void create_json_data(char *json_data, size_t size, struct MemData memoryData) {
-    snprintf(json_data, size,
-             "{\"totalram\":%d, \"freeram\":%d}",
-             memoryData.total / 1048576,
-             memoryData.free / 1048576);
-}
